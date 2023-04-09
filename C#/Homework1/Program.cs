a7fe47fe96759aa1292f1e32135a7ce30f76d8cc
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-09 13:21:48 +0300

    домашнее задание по первому семинару C#

---
+++ C#/Homework1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 2
+Console.Write("Введите превое число ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+if(a > b){
+    Console.WriteLine($"Максимальное значение {a}");
+}
+else{
+    Console.WriteLine($"Максимальное значение {b}");
+}
+// Задача 4
+Console.Write("Введите превое число ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int d = int.Parse(Console.ReadLine());
+Console.Write("Введите Третье число ");
+int e = int.Parse(Console.ReadLine());
+if(c > d){
+    if(c > e){
+    Console.WriteLine($"Максимальное значение {c}");    
+    }
+    else
+    Console.WriteLine($"Максимальное значение {e}");
+}
+else{
+    if(d > e){
+    Console.WriteLine($"Максимальное значение {d}");    
+    }
+    else
+    Console.WriteLine($"Максимальное значение {e}");
+}
+// задача 6
+Console.Write("Введите число ");
+int f = int.Parse(Console.ReadLine());
+if(f % 2 == 0){
+    Console.WriteLine("Да");    
+}
+else{
+    Console.WriteLine("Нет");    
+}
+// задача 8
+int count = 1;
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine());
+while(count < N){
+    if(count % 2 == 0){
+        Console.WriteLine(count);
+    }
+count++;
+}
---
+++ C#/Program.cs
@@ -0,0 +1,32 @@
+﻿// int N = int.Parse(Console.ReadLine());
+// if(N<0) N = N*-1;
+// for (int i = -N; i <= N; i++)
+// {
+//     Console.WriteLine(i);
+// }
+//int M = int.Parse(Console.ReadLine())%10;
+//Console.WriteLine(M);
+double count = 0;
+double distance = 10000;
+double firstfriendspeed = 1;
+doub
+le secondfriendspeed = 2;
+double dogspeed = 5;
+double friend = 2;
+double time = 0;
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstfriendspeed+dogspeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondfriendspeed+dogspeed);
+        friend = 2;
+    }
+    distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+    count = count + 1;
+}
+Console.Write(count);
