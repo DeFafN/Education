95e3255ed47d6695088e9c22308182af4d0e4d1f
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-15 10:14:44 +0300

    решены все задачи

---
+++ C#/Homework2/Program.cs
@@ -0,0 +1,35 @@
+﻿// задача 1
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int first = number / 10;
+int second =  first % 10;
+Console.WriteLine("Второая цифра: "+ second);
+
+// задача 2
+Console.WriteLine("Введите число");
+int number2 = int.Parse(Console.ReadLine());
+if (number2>999)
+{
+    string convert_number2 = number2.ToString();
+    Console.WriteLine("Третья цифра: " + convert_number2[2]);
+}
+else
+{
+    if(number2<100){
+        Console.WriteLine("Третьей цифры нет");
+    }    
+    else {
+        int vtoraya = number2 % 10;
+        Console.WriteLine("Третья цифра: " + vtoraya);
+    }
+}
+
+// задача 3
+Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 | day == 7){
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
