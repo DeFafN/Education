f67797891dea5a1bb1be2263c843ec064f042e75
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-17 23:56:49 +0300

    решена задача с нахождением расстояния в 3-d пространством

--- C#/Homework3/Program.cs
+++ C#/Homework3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты X1");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y1");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z1");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты X2");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y2");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z2");
+double z2 = double.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+Console.WriteLine($"Расстояние равно {distance}");
