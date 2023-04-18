71c684fc2544d43c2744498f8b19481d7b127081
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-18 19:33:51 +0300

    полиндром

--- C#/Homework3/Program.cs
+++ C#/Homework3/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите пятизначное число");
 string number = Console.ReadLine();
-string reverse_number = number.Reverse(number);
-if (number.Lehgth() != 5)
+int Length = number.Length;
+if (number.Length != 5)
 {
     Console.WriteLine("Количество знаков не равно 5");
 }
-    else if (number == reverse_number)
+    else if (number[0] == number[4] && number[1] == number[3])
     {
         Console.WriteLine("Число является полиндромом");
     }
@@ -13,3 +13,19 @@ if (number.Lehgth() != 5)
     {
         Console.WriteLine("Число не является полиндромом");
     }
+// Неработающий код, хотелось бы узнать почему
+// Console.WriteLine("Введите пятизначное число");
+// string number = Console.ReadLine();
+// string reverse_number = number.Reverse();
+// if (number.Length != 5)
+// {
+//     Console.WriteLine("Количество знаков не равно 5");
+// }
+//     else if (number == reverse_number)
+//     {
+//         Console.WriteLine("Число является полиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не является полиндромом");
+//     }
