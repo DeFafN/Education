f23d8e7c9eef60c7fc64f29b5732ddf8d625064c
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-22 13:05:43 +0300

    возведение А в степень В

--- C#/Homework4/Program.cs
+++ C#/Homework4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Pow (int Base, int index)
+{
+int result = Base;
+if (index == 0)
+    {
+        return 1;
+    }
+else
+    {
+        for (int i = 0; i < index-1; i++)
+        {
+            Base = Base * result;
+        }
+    return Base;    
+}
+}
+Console.Write("введите число которое необходимо возвести в степень ");
+int Base = int.Parse(Console.ReadLine());
+Console.Write("введите степень в которую необходимо возвести ");
+int index = int.Parse(Console.ReadLine());
+Console.WriteLine("результат операции: " + Pow(Base, index));
