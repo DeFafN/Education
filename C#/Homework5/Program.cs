8cb1bbb85816acb672b834b1173b6646a9e6af85
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-23 16:47:29 +0300

    вопрос

--- C#/Homework5/Program.cs
+++ C#/Homework5/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+void Fill(int[] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(100, 1000);
+        }
+}
+
+void Print(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+int Even(int[] numbers)
+{
+    int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (i % 2 == 0)
+            count+=
+        }
+    return count;
+}
+Fill(numbers);
+Console.WriteLine("созданный массив:");
+Print(numbers);
+Even(numbers);
