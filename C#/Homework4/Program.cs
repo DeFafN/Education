38e46fcd02ec059c14b6a9eef71c56db8401e93f
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-25 22:38:49 +0300

    123

--- C#/Homework4/Program.cs
+++ C#/Homework4/Program.cs
@@ -1,22 +1,6 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-int Pow (int Base, int index)
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int numbers(int number)
 {
-int result = Base;
-if (index == 0)
-    {
-        return 1;
-    }
-else
-    {
-        for (int i = 0; i < index-1; i++)
-        {
-            Base = Base * result;
-        }
-    return Base;    
+    return numbers.Length;
 }
-}
-Console.Write("введите число которое необходимо возвести в степень ");
-int Base = int.Parse(Console.ReadLine());
-Console.Write("введите степень в которую необходимо возвести ");
-int index = int.Parse(Console.ReadLine());
-Console.WriteLine("результат операции: " + Pow(Base, index));
+numbers(52);
