92fe7f46aa8e7dccf942aae4935d401744e1a668
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-04-25 20:49:38 +0300

    две задачи

--- C#/Homework5/Program.cs
+++ C#/Homework5/Program.cs
@@ -1,16 +1,25 @@
-﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2
+﻿string input (string text)
+{
+    Console.Write($"{text}");
+    return Console.ReadLine();
+}
 
-Console.WriteLine("Введите размер массива");
-int size = int.Parse(Console.ReadLine());
-int[] numbers = new int[size];
-void Fill(int[] numbers)
+void Fill_random_thre_digit_positive_numbers(int[] numbers)
 {
     Random rnd = new Random();
     for (int i = 0; i < numbers.Length; i++)
         {
             numbers[i] = rnd.Next(100, 1000);
+        }
+}
+
+void Fill_user_numbers(int[] numbers, int minvalue = -1000, int maxvalue = 1000)
+{
+    Random rnd = new Random();
+    maxvalue++;
+    for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(minvalue, maxvalue);
         }
 }
 
@@ -22,6 +31,7 @@ void Print(int[] numbers)
     }
     Console.WriteLine();
 }
+
 int Even(int[] numbers)
 {
     int count = 0;
@@ -32,7 +42,51 @@ int Even(int[] numbers)
         }
     return count;
 }
-Fill(numbers);
-Console.WriteLine("созданный массив:");
-Print(numbers);
-Console.WriteLine(Even(numbers));
+
+int sum_not_even_numbers (int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            result = result + numbers[i];
+        }
+    }
+    return result;    
+}
+
+// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+void Even_in_Array()
+{
+    Console.WriteLine("Введите размер массива");
+    int size = int.Parse(Console.ReadLine());
+    int[] numbers = new int[size];
+    Fill_random_thre_digit_positive_numbers(numbers);
+    Console.WriteLine("Созданный массив:");
+    Print(numbers);
+    Console.Write("Количество чётных чисел: ");
+    Console.WriteLine(Even(numbers));
+}
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+void not_even_index()
+{
+    Console.WriteLine("Введите размер массива");
+    int size = int.Parse(Console.ReadLine());
+    int[] numbers = new int[size];
+    Fill_user_numbers(numbers, -100, 500);
+    Console.WriteLine("Созданный массив:");
+    Print(numbers);
+    Console.WriteLine($"Сумма нечётных элементовсозданного массива: {sum_not_even_numbers(numbers)}");
+}
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3,21 7,04 22,93 -2,71 78,24] -> 80,95
+
+
+
+
+
