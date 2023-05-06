86b8a78e91473c213107f08679618c39caee6ecc
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-05-06 12:31:51 +0300

    task47

--- C#/Homework8/Program.cs
+++ C#/Homework8/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Введите размер 1-го измерения массива");
+int firstLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер 2-го измерения массива"); 
+int secondLength = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[firstLength, secondLength];
+Random rnd = new Random();
+
+double[,] CreateTwoDimensionArray(double[,] array)
+{
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
+             array[i, j] = Math.Round(rnd.NextDouble() * 100 - 50, 1);
+
+    return array;
+}
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+PrintArray(CreateTwoDimensionArray(array));
