52ee82cba5b19f6b42ca9a365b63b918f6e44d2d
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-06-03 13:46:05 +0300

    1

---
+++ C#/Homework10/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+void PrintDigit(int digit)
+{
+    Console.WriteLine(digit);
+    if (digit > 1)
+    {
+       PrintDigit(digit - 1);
+    }
+}
+PrintDigit(2);
+Console.WriteLine();
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30*/
+
+int SumOfElements(int firstDigit, int secondDigit)
+{
+    if (firstDigit < secondDigit)
+    {
+       return firstDigit + SumOfElements(firstDigit + 1, secondDigit);
+    }
+ return firstDigit;
+}
+Console.WriteLine(SumOfElements(4, 8));
+Console.WriteLine();
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 29*/
+
+int Akerman (int numberOne , int numberTwo)
+{
+    if (numberOne == 0)
+        return numberTwo + 1;
+    else if (numberTwo == 0)
+        return numberOne + 1;
+    else 
+        return Akerman(numberOne-1, Akerman(numberOne, numberTwo-1));
+}
+Console.WriteLine(Akerman(3,2));
