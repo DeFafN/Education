490924b4913d1763522e54130b1c31e02f19a3b9
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-08-12 10:49:38 +0300

    как-то так

---
+++ Python/seminar6/seminar6.py
@@ -0,0 +1,25 @@
+﻿# Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+element = int(input('Введите первый элемент прогрессии: '))
+lenList = int(input('Введите количесвто элементов: '))
+step = int(input('Введите разность между элементами: '))
+arithmeticProgression = []
+arithmeticProgression.append(element)
+print(arithmeticProgression)
+for i in range(lenList): 
+    element = element + step
+    arithmeticProgression.append(element)
+print(arithmeticProgression)
+
+
+# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+# т.е. не меньше заданного минимума и не больше заданного максимума
+
+numList = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+indexList = []
+for element in numList:
+    if element > 5 and element <15:
+      indexList.append(numList.index(element))
+print(f'Индексы элементов в диапозоне от 5 до 15 {indexList}')
