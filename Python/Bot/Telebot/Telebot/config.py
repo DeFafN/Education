ab906ac23cd585da456c861b2e5938fc5c045f90
DeFafN <129111586+DeFafN@users.noreply.github.com>
2023-09-17 17:21:37 +0300

    nry again

---
+++ Python/Bot/Telebot/Telebot/config.py
@@ -0,0 +1,4 @@
+import telebot
+
+token = '6639685164:AAE8fej-OdKOCZcTZ9R0AIbyy0fkUu1gZNg'
+bot = telebot.TeleBot(token)
--- Python/Bot/Telebot/Telebot/main.py
+++ Python/Bot/Telebot/Telebot/main.py
@@ -1,6 +1,9 @@
-﻿from bot import bot
+﻿import telebot
+from telebot import types
+from config import bot
 
-profile = '' 
+
+PROFILE = '' 
 name = ''
 location = '' 
 salary = ''
@@ -9,7 +12,7 @@ stack = ''
 resume = '' 
 coment = '' 
 hashtag = ''
-candidat = profile + name + location + salary + work_mode + stack + resume + coment + hashtag
+CANDIDAT = PROFILE + name + location + salary + work_mode + stack + resume + coment + hashtag
 # user_data = {profile : '', name : '', location : '', salary : '', work_mode : '', stack : '', resume: '', coment : '', hashtag : ''}
 
 HELP = """ 
@@ -17,7 +20,6 @@ HELP = """
 /start - отправить информацию по кандидату
 
 """
-@bot.message_handler(content_types=['text'])
 
 @bot.message_handler(commands = ['start', 'help'])
 def commands(message):
@@ -30,61 +32,65 @@ def commands(message):
         bot.send_message(message.chat.id, "Напиши /help или /start")
 
 def get_profile(message):
-    global profile
     profile = message.text
     bot.send_message(message.from_user.id, 'Напиши фамилию и инициалы кандидата')
-    bot.register_next_step_handler(message, get_name)
-    
-def get_name(message):
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, 'Из какого города кандидат?')
-    bot.register_next_step_handler(message, get_location)
-
-def get_location(message):
-    global location
-    location = message.text
-    bot.send_message(message.from_user.id, 'Какие зарплатные ожидания у кандидата?')
-    bot.register_next_step_handler(message, get_salary)
-
-def get_salary(message):
-    global salary
-    salary = message.text
-    bot.send_message(message.from_user.id, 'Какие форматы работы рассматривает кандидат')
-    bot.register_next_step_handler(message, get_work_mode)
-
-def get_work_mode(message):
-    global work_mode
-    work_mode = message.text
-    bot.send_message(message.from_user.id, 'Укажи стэк кандидата')
-    bot.register_next_step_handler(message, get_stack)
-
-def get_stack(message):
-    global stack
-    stack = message.text
-    bot.send_message(message.from_user.id, 'Укажи ссылку на резюме кандидата')
-    bot.register_next_step_handler(message, get_resume)
-
-def get_resume(message):
-    global resume
-    resume = message.text
-    bot.send_message(message.from_user.id, 'комментарий')
-    bot.register_next_step_handler(message, get_coment)
-
-def get_coment(message):
-    global coment
-    coment = message.text
-    bot.send_message(message.from_user.id, 'хэштег')
-    bot.register_next_step_handler(message, get_hashtag)
-
-def get_hashtag(message):
-    global hashtag
-    hashtag = message.text
-    bot.send_message(message.from_user.id, 'Всё верно?')
+    global PROFILE
+    PROFILE = profile
     bot.register_next_step_handler(message, check)
-
-def check (message):
-    global candidat
+print(PROFILE)
+# def get_name(message):
+#     global name
+#     name = message.text
+#     bot.send_message(message.from_user.id, 'Из какого города кандидат?')
+#     bot.register_next_step_handler(message, get_location)
+# print(name)
+# def get_location(message):
+#     global location
+#     location = message.text
+#     bot.send_message(message.from_user.id, 'Какие зарплатные ожидания у кандидата?')
+#     bot.register_next_step_handler(message, get_salary)
+# print(location)
+# def get_salary(message):
+#     global salary
+#     salary = message.text
+#     bot.send_message(message.from_user.id, 'Какие форматы работы рассматривает кандидат')
+#     bot.register_next_step_handler(message, get_work_mode)
+# print(salary)
+# def get_work_mode(message):
+#     global work_mode
+#     work_mode = message.text
+#     bot.send_message(message.from_user.id, 'Укажи стэк кандидата')
+#     bot.register_next_step_handler(message, get_stack)
+# print(work_mode)
+# def get_stack(message):
+#     global stack
+#     stack = message.text
+#     bot.send_message(message.from_user.id, 'Укажи ссылку на резюме кандидата')
+#     bot.register_next_step_handler(message, get_resume)
+# print(stack)
+# def get_resume(message):
+#     global resume
+#     resume = message.text
+#     bot.send_message(message.from_user.id, 'комментарий')
+#     bot.register_next_step_handler(message, get_coment)
+# print(resume)
+# def get_coment(message):
+#     global coment
+#     coment = message.text
+#     bot.send_message(message.from_user.id, 'хэштег')
+#     bot.register_next_step_handler(message, get_hashtag)
+# print(coment)
+# def get_hashtag(message):
+#     global hashtag
+#     hashtag = message.text
+#     bot.send_message(message.from_user.id, 'Всё верно?')
+#     bot.register_next_step_handler(message, check)
+# print(hashtag)
+def check (message, candidat=CANDIDAT):
     bot.send_message(message.chat.id, f'{candidat}')
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.from_user.id, "Напиши /help")
 
 bot.polling()
